158edf7b929bea9a502382973a215633061ad651
[myblog] <imanlintag13@mittymonarch.com>
2017-07-17 18:15:26 -0700

    Spawn in ball and basketball

--- Mega_Shot/ViewController.swift
+++ Mega_Shot/ViewController.swift
@@ -69,4 +69,39 @@ class ViewController: UIViewController {
         basketCollision.addBoundary(withIdentifier: "leftPanier" as NSCopying, from: CGPoint(x: 135, y: 268), to: CGPoint(x: 136, y: 268))
         basketCollision.addBoundary(withIdentifier: "rightPanier" as NSCopying, from: CGPoint(x: 231, y: 268), to: CGPoint(x: 232, y: 268))
     }
+
+    func spawnPanierLine() {
+        if progBasketLine != nil {
+            progBasketLine.removeFromSuperview()
+            progBasketLine = nil
+        }
+        
+        progBasketLine = UIImageView(image: UIImage(named: "line"))
+        
+        let xPosition = basket.frame.origin.x + basket.frame.width - 154
+        let yPosition = basket.frame.origin.y + basket.frame.height - 26.5
+        let newFrame = CGRect(x: xPosition, y: yPosition, width: 97.5, height: 6)
+        
+        progBasketLine.frame = newFrame
+        view.addSubview(progBasketLine)
+    }
+    
+    func spawnBall() {
+        if progBasketball != nil {
+            progBasketball.removeFromSuperview()
+            progBasketball = nil
+        }
+        
+        let randMax: Int = Int(self.view.frame.size.width - 80)
+        let xPosition = arc4random_uniform(UInt32(randMax))
+        let xPositionFloat : CGFloat = CGFloat(xPosition)
+        let newFrame = CGRect(x: xPositionFloat, y: 567.0, width: 80.0, height: 80.0)
+        
+        progBasketball = Ball(image: UIImage(named: "basketball"))
+        progBasketball.frame = newFrame
+        view.addSubview(progBasketball)
+        
+        createDynamicProperties()
+    }
+
 }
